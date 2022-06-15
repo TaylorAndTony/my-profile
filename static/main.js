bb31ddbc5c249f12da8f385aca90d227ecfb4052
TaylorAndTony <doyijian@163.com>
2022-06-15 12:55:43 +0800

    add more

--- static/main.js
+++ static/main.js
@@ -13,6 +13,14 @@ const App = {
                 {name: 'Auditon', icon: 'img/au.png'},
                 {name: 'Illustrator', icon: 'img/ai.png'},
             ],
+            codeSkills: [
+                {name: 'HTML', icon: 'img/h5.png'},
+                {name: 'HTML', icon: 'img/css.png'},
+                {name: 'HTML', icon: 'img/js.png'},
+                {name: 'Vue', icon: 'img/vue.png'},
+                {name: 'C', icon: 'img/c.png'},
+                {name: 'Python', icon: 'img/py.png'},
+            ]
         }
     }
 }
